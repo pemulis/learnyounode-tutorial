86602a5aec9a9fda5e1bc28dec1b5a46e00d5c3a
pemulis <john.d.shutt@gmail.com>
2015-04-22 12:28:35 -0700

    add solutions for HTTP Client

---
+++ http_client.js
@@ -0,0 +1,28 @@
+var http = require('http');
+
+var url = process.argv[2];
+
+/* Original solution. */
+
+/*
+function req(url) {
+  http.get(url, function callback (response) {
+    response.on("data", function (data) {
+      console.log(data);
+    }).setEncoding('utf8');
+    response.on('error', function (err) {
+      console.log('There was an error: ' + err)
+    });
+  });
+}
+
+req(url);
+*/
+
+/* Cleaned up based on official solution. */
+
+http.get(url, function (response) {
+  response.setEncoding('utf8');
+  response.on('data', console.log);
+  response.on('error', console.error);
+});
