a9a2b6978d351bbd94c44cb72b2e449c04c7b2d5
John Shutt <jds@riseup.net>
2015-04-26 23:54:46 -0700

    update File Server code

--- file_server.js
+++ file_server.js
@@ -8,3 +8,20 @@ var server = http.createServer(function (req, res) {
   fs.createReadStream(filepath).pipe(res);
 });
 server.listen(port);
+
+/* Official solution for reference. */
+
+/*
+
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+
+  fs.createReadStream(process.argv[3]).pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
+
+*/
