0fbd0307af4f3de1c3783150ff48d7ef4e5d5aa0
John Shutt <jds@riseup.net>
2015-04-27 21:41:52 -0700

    add JSON API solution

--- json_api.js
+++ json_api.js
@@ -2,19 +2,36 @@ var http = require('http');
 var url = require('url');
 var port = process.argv[2];
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
 
+function unixtime (time) {
+  return { unixtime : time.getTime() }
+}
+
+var server = http.createServer(function (req, res) {
   var parsed = url.parse(req.url, true);
   var pathname = parsed['pathname'];
+  var time = new Date(parsed.query.iso);
+  var result;
 
   if (pathname === '/api/unixtime') {
-    //do stuff
+    result = unixtime(time);
   } else if (pathname === '/api/parsetime') {
-    //do different stuff
+    result = parsetime(time);
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify(result));
   } else {
-    return 'Invalid request.\n';
+    res.writeHead(404);
+    res.end();
   }
 });
+server.listen(Number(port));
